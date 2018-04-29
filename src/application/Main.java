f5f0855b53bd139dfa461e6247643c5fa06d8ce4
Yanzheng Li <yanzheng@YanzhengdeMacBook-Air.local>
2018-04-29 13:21:04 -0500

    updated reorder

--- src/application/Main.java
+++ src/application/Main.java
@@ -37,6 +37,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+    
     	try {
             teamsName = Files.lines(Paths.get("teams"))
                             .map(String::trim)
