cce160eaf7cb68217bcc16ce9422109f39c7a375
Harvey <36828392+HarveyWangWisc@users.noreply.github.com>
2018-04-29 15:06:48 -0500

    Update Main.java
    commenting

--- src/application/Main.java
+++ src/application/Main.java
@@ -39,6 +39,7 @@ public class Main extends Application {
     public static void main(String[] args) {
     
     	try {
+            // read teams
             teamsName = Files.lines(Paths.get("/Users/yanzheng/eclipse-workspace/Bracket/src/teams"))
                             .map(String::trim)
                             .filter(x->x!=null && !x.equals(""))
