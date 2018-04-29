769b085b391a859084fe40534ab1d75dceb0f719
Harvey <36828392+HarveyWangWisc@users.noreply.github.com>
2018-04-29 15:56:01 -0500

    Update AlertBox.java

--- src/application/AlertBox.java
+++ src/application/AlertBox.java
@@ -1,3 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Title:            Tournament Bracket
+// Files:            Main.java, AlertBox.java, Bracket.java, GraphGenerateor.java, Team.java, VSButton.java, application.css, style.css, background2.jpg
+// Semester:         CS400 Spring 2018
+//
+// Author:           Weiheng Wang wwang522@wisc.edu
+//                   Haoran Li hli584@wisc.edu
+//                   Yuming Ma  ma226@wisc.edu
+//                   Yanzheng Li li694@wisc.edu
+//                   Taijing Chen tchen284@wisc.edu
+//
+//
+//                   
+// Lecturer's Name:  Deb Deppeler
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 package application;
 
 import javafx.geometry.Insets;
