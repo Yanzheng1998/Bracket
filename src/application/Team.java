82d3c12ee74d4c3800ec223860568d4ff8f4f227
Harvey <36828392+HarveyWangWisc@users.noreply.github.com>
2018-04-29 15:09:06 -0500

    Update Team.java

--- src/application/Team.java
+++ src/application/Team.java
@@ -28,7 +28,7 @@ public class Team {
         teamBox.setMaxHeight(0);
 
 
-
+        // set the score text field invisible by default
         score.setVisible(false);
         score.setPrefWidth(80);
 
