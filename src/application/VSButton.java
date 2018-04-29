02633f4bdbb0a31a478546b6733578d954339b75
Haoran Li <32046703+Neptune584@users.noreply.github.com>
2018-04-29 14:33:10 -0500

    Update VSButton.java

--- src/application/VSButton.java
+++ src/application/VSButton.java
@@ -8,46 +8,55 @@ import javafx.scene.layout.HBox;
 
 public class VSButton {
 
-	public Button vsButton;
-	public HBox buttonBox;
+    public Button vsButton;
+    public HBox buttonBox;
 
-	public VSButton(Bracket bracket, Team[][] teams, int seriesNumber, int teamNum, int region) {
-		vsButton = new Button("VS");
-		buttonBox = new HBox();
-		// set action
-		vsButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				String name1 = teams[seriesNumber - 1][teamNum * 2 - 1].name.getText();
-				String name2 = teams[seriesNumber - 1][teamNum * 2].name.getText();
-				String score1 = teams[seriesNumber - 1][teamNum * 2 - 1].score.getText();
-				String score2 = teams[seriesNumber - 1][teamNum * 2].score.getText();
-				Integer scoreNum1 = Integer.valueOf(score1);
-				Integer scoreNum2 = Integer.valueOf(score2);
-				if (score1 != null && score2 != null) {
-					if (scoreNum1 > scoreNum2) {
-						teams[seriesNumber][teamNum].name.setText(name1);
-						teams[seriesNumber][teamNum].score.setVisible(true);
-						// is this button is the semi-final game button
-						if (seriesNumber == bracket.roundNum) {
-							bracket.thirdPlaceName[region] = name2;
-							bracket.thirdPlaceScore[region] = scoreNum2;
-						}
-					} else {
-						teams[seriesNumber][teamNum].name.setText(name2);
-						teams[seriesNumber][teamNum].score.setVisible(true);
-						// is this button is the semi-final game button
-						if (seriesNumber == bracket.roundNum) {
-							bracket.thirdPlaceName[region] = name1;
-							bracket.thirdPlaceScore[region] = scoreNum1;
-						}
-					}
-				}
-			}
-		});
-		buttonBox.getChildren().add(vsButton);
-		buttonBox.setAlignment(Pos.CENTER);
+    public VSButton(Bracket bracket, Team[][] teams, int seriesNumber, int teamNum, int region) {
+        vsButton = new Button("VS");
+        buttonBox = new HBox();
+        // set action
+        vsButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                String name1 = teams[seriesNumber - 1][teamNum * 2 - 1].name.getText();
+                String name2 = teams[seriesNumber - 1][teamNum * 2].name.getText();
+                String score1 = teams[seriesNumber - 1][teamNum * 2 - 1].score.getText();
+                String score2 = teams[seriesNumber - 1][teamNum * 2].score.getText();
+                try {
+                    Integer scoreNum1 = Integer.valueOf(score1);
+                    Integer scoreNum2 = Integer.valueOf(score2);
+                    if (score1 != null && score2 != null) {
+                        if (scoreNum1 > scoreNum2) {
+                            teams[seriesNumber][teamNum].name.setText(name1);
+                            teams[seriesNumber][teamNum].score.setVisible(true);
+                            // is this button is the semi-final game button
+                            if(seriesNumber==bracket.roundNum) {
+                                bracket.thirdPlaceName[region] = name2;
+                                bracket.thirdPlaceScore[region] = scoreNum2;
+                            }
+                        } else {
+                            teams[seriesNumber][teamNum].name.setText(name2);
+                            teams[seriesNumber][teamNum].score.setVisible(true);
+                            // is this button is the semi-final game button
+                            if(seriesNumber==bracket.roundNum) {
+                                bracket.thirdPlaceName[region] = name1;
+                                bracket.thirdPlaceScore[region] = scoreNum1;
+                            }
+                        }
+                    }
+                } catch (NumberFormatException r) {
+                    if (score1.isEmpty() || score2.isEmpty())
+                        AlertBox.display("WARNING", "Score entered is empty");
+                    else
+                        AlertBox.display("WARNING", "Please enter integers as team score");
+                }
+            }
+        });
+        buttonBox.getChildren().add(vsButton);
+        buttonBox.setAlignment(Pos.CENTER);
 
-	}
+    }
+
+
 
 }
